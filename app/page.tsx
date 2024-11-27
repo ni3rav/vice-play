0dd13c2129b9a72b80bcdbf08c1fbb9acaf0abac
ni3rav <mahetan35@gmail.com>
2024-11-27 11:01:07 +0530

    done with the music player frontend

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,23 @@
+import { Player } from "@/components/Player";
+
 function page() {
+  const songs = [
+    { title: "Awesome 80s Mix", src: "/songs/Billie Jean.mp3" },
+    { title: "Synthwave Nights", src: "/public/songs/Blinding Lights.mp3" },
+  ];
   return (
     <div className="h-screen w-full flex justify-center items-center">
+      <Player
+        // isPlaying={isPlaying}
+        // playSong={playSong}
+        // pauseSong={pauseSong}
+        // nextSong={nextSong}
+        // previousSong={previousSong}
+        // isBuffering={isBuffering}
+        // hasStarted={hasStarted}
+        // currentSong={currentSongIndex}
+        songs={songs}
+      />
     </div>
   );
 }
